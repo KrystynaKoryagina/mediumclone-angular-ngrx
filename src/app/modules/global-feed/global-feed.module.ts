b7c1ce266b58b310aa1e378b1231f5e663fa5025
Krystyna <krystyna.koryagina@gmail.com>
2021-02-01 09:38:52 +0200

    add banner component

--- src/app/modules/global-feed/global-feed.module.ts
+++ src/app/modules/global-feed/global-feed.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { GlobalFeedComponent } from './components/global-feed/global-feed.component';
 import { GlobalFeedRoutingModule } from './global-feed-routing.module';
 import { FeedModule } from '../common/modules/feed/feed.module';
+import { BannerModule } from '../common/modules/banner/banner.module';
 
 @NgModule({
   declarations: [GlobalFeedComponent],
-  imports: [CommonModule, GlobalFeedRoutingModule, FeedModule],
+  imports: [CommonModule, GlobalFeedRoutingModule, FeedModule, BannerModule],
 })
 export class GlobalFeedModule {}
